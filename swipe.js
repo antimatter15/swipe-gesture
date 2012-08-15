957fd9ad198f5648f571db24f4e8d14461e506b4
antimatter15 <antimatter15@gmail.com>
2012-08-15 11:26:09 -0400

    dynamic pause durations, removed smoothing

--- swipe.js
+++ swipe.js
@@ -1,5 +1,43 @@
 
+function signalCompletion(){
+	console.log('asdfja9ofjawerjwoaer', direction)
+	// document.body.style.backgroundColor = '#B9D3B9';
+}
+function signalCancellation(){
+	// document.body.style.backgroundColor = '#D1A5A5';
+}
+function signalScroll(){
+	console.log('you see me scrolling') //my front lawn
+	//i know you're all thinking he's so white and nerdy
+	// document.body.style.backgroundColor = '#D7DB7F';
+}
+function signalEnd(){
+	console.log("DONE")
+	// setTimeout(function(){
+	// 	document.body.style.backgroundColor = '';
+	// }, 500)
+}
+function transformLength(x){ //x is a positive real number
+	// return Math.sqrt(x)
+	// return 1
+	return x
+}
+
+
+var orientations = [180, 0, 315, 225, 90, 270, 135, 45];
+var LENGTH_THRESHOLD = 1000;
+var XY_SPLIT = 0.5; //50-50
+var INVERT_ARROW = false;
+
+
+
 function drawArrow(canvas, orientation){
+	//parameters of drawing an arrow
+	var shaft_rad = 5, 
+		point_len = 15, 
+		head_rad = 15, 
+		shaft_len = 30;
+
 	// orientation = -orientation;
 	orientation = (2 * 360 + (orientation || 0)) % 360;
 
@@ -11,7 +49,7 @@ function drawArrow(canvas, orientation){
 	c.beginPath()
 	c.fillStyle = 'white';
 
-	var shaft_rad = 5, point_len = 15, head_rad = 15, shaft_len = 30;
+
 
 	c.translate(25, 25)
 	// c.scale(-1, 1)
@@ -48,8 +86,7 @@ function createArrow(){
 	canvas.style.padding = '10px';
 
 	canvas.style.webkitTransitionProperty = 'opacity, -webkit-transform, transform, top, left, right, bottom';
-	canvas.style.webkitTransitionDuration = '0.5s, 0.5s, 0.2s, 0.2s, 0.2s, 0.2s'	
-
+	canvas.style.webkitTransitionDuration = '0.5s, 0.5s, 0.2s, 0.2s, 0.2s, 0.2s'
 	canvas.style.position = 'absolute';
 	canvas.style.zIndex = '9999999999999999';
 	
@@ -59,17 +96,56 @@ function createArrow(){
 	return canvas;
 }
 
+function renderProgress(pct){
+	if(!navArrow) return; //nav arrow is necessary
+	if(pct == 0){
+		navArrow.style.opacity = pct;
+	}else{
+		navArrow.style.opacity = pct * 0.4 + 0.1;	
+	}
+
+	if(INVERT_ARROW){
+		pct = 1 - pct;	
+	}
+	
+
+	var boxEdge = 70;
+	var nW = ((innerWidth + boxEdge) * pct - boxEdge) + 'px';
+	var nH = ((innerHeight + boxEdge) * pct - boxEdge) + 'px';
+
+	if(direction == 180 || direction == 0){
+		navArrow.style.top = (innerHeight / 2 - boxEdge / 2) + 'px';
+	}else if(direction == 270 || direction == 90){
+		navArrow.style.left = (innerWidth / 2 - boxEdge / 2) + 'px';
+	}
+	//these are for the diags
+	if(direction == 315 || direction == 225 || direction == 270){ // UP
+		navArrow.style.bottom = nH;
+	}else if(direction == 135 || direction == 45 || direction == 90){ //DOWN
+		navArrow.style.top = nH;
+	}
+	if(direction == 315 || direction == 45 || direction == 0){ //RIGHT
+		navArrow.style.left = nW;
+	}else if(direction == 225 || direction == 135 || direction == 180){ //LEFT
+		navArrow.style.right = nW;
+	}
+}
+
 
 var scrollListened = [];
 var lastDetectedScroll = 0;
-// var isTriggering = 0;
 var wheelBuffer = [];
 var lastWheelTimer = -1;
 var direction;
 var navArrow;
+var len = 0;
+var currentSession = -1;
+// 0 = unknown/not begun
+// 1 = scroll
+// 2 = MAGICALPONIES
 
 function detectScroll(e){
-	console.log("detected scroll on", e.target, e, +new Date)
+	// console.log("detected scroll on", e.target, e, +new Date)
 	lastDetectedScroll = +new Date;
 	removeListeners();
 }
@@ -81,88 +157,38 @@ function removeListeners(){
 	scrollListened = [];
 }
 
-var len = 0;
 function mouseWheel(x, y){
-	x /= 2;
-	y /= 2;
-
-	var dp = Math.cos(direction / 180 * Math.PI) * x + Math.sin(direction / 180 * Math.PI) * y;
-	
-	len += dp;
-
 	//dot product of direction and the stuff, why, i dont know im dumb
+	var dp = XY_SPLIT * Math.cos(direction / 180 * Math.PI) * x + (1-XY_SPLIT) * Math.sin(direction / 180 * Math.PI) * y;
+	
+	//give transformLength a positive thing just cause
+	len += transformLength(Math.abs(dp)) * (dp < 0 ? -1 : 1);
+
 	// x = x / 500 * innerWidth
 	// y = y / 500 * innerHeight
-	var pct = Math.max(0, Math.min(1, len / 500));
+	var pct = Math.max(0, Math.min(1, len / LENGTH_THRESHOLD));
 	// console.log(dp, len, pct);
 	renderProgress(pct);
 
-	if(pct >= 0.75){
-		// navArrow.style.backgroundColor = 'blue'
+	if(pct >= 0.80){
 		//DONE!
 		removeArrow(navArrow, true);
-		// endTrigger()
-		signalCompletion();
+		if(currentSession != 3){
+			signalCompletion();	
+		}
+		
 		currentSession = 3;
-		
-	}
-	// console.log(x, y)
-}
-
-// var lastCompletion = 0;
-function signalCompletion(){
-	if(currentSession != 3){
-		console.log('asdfja9ofjawerjwoaer', direction)
-		// document.body.style.backgroundColor = '#B9D3B9';
 	}
 }
-
-function renderProgress(pct){
-	if(navArrow){
-		// console.log(pct)
-		// console.log(navArrow.style.left)
-		if(pct == 0){
-			navArrow.style.opacity = 0;
-		}else{
-			navArrow.style.opacity = pct * 0.4 + 0.1;	
-		}
-		
-		if(direction == 180 || direction == 0){
-			navArrow.style.top = (innerHeight / 2 - 25) + 'px';
-		}else if(direction == 270 || direction == 90){
-			navArrow.style.left = (innerWidth / 2 - 25) + 'px';
-		}
-		// pct = 1 - pct 
-		if(direction == 180){ //LEFT
-			// navArrow.style.left = (innerWidth * ( 1 - pct) - 70) + 'px';
-			navArrow.style.right = innerWidth * pct + 'px';
-		}else if(direction == 0){ //RIGHT
-			navArrow.style.left = innerWidth * pct + 'px';
-		}else if(direction == 270){ //UP
-			// navArrow.style.top = (innerHeight * (1-pct) - 70) + 'px';
-			navArrow.style.bottom = innerHeight * pct + 'px';
-		}else if(direction == 90){ //DOWN
-			navArrow.style.top = innerHeight * pct + 'px';
-		}
-
-	}
-}
-
-
-var currentSession = -1;
-// 0 = unknown/not begun
-// 1 = scroll
-// 2 = MAGICALPONIES
 
 function removeArrow(arrow, complete){
-
 	if(arrow && arrow.style){
 		if(complete){
 			renderProgress(1)	
 		}else if(currentSession != 3){
 			renderProgress(0)
 			arrow.style.webkitTransform = 'scale(2.0)';
-			// document.body.style.backgroundColor = '#D1A5A5';
+			signalCancellation()
 		}
 		
 		arrow.addEventListener('webkitTransitionEnd', function(){
@@ -174,34 +200,43 @@ function removeArrow(arrow, complete){
 
 		})
 	}
+	if(arrow === navArrow){
+		navArrow = null;
+	}
 }
 
 function endTrigger(){
 	if(navArrow){
 		navArrow.style.webkitTransitionDuration = '0.5s, 0.5s, 0.5s, 0.5s, 0.5s, 0.5s'	
 	}
-	
-
+	signalEnd();
 	removeArrow(navArrow);
 	currentSession = -1;
 	wheelBuffer = [];
 	len = 0;
-	navArrow = null;
+	
 	clearTimeout(lastWheelTimer);
-	// setTimeout(function(){
-	// 	document.body.style.backgroundColor = '';
-	// }, 500)
 	
-	
-	console.log("DONE")
 }
 
-// var orientations = [180, 0, 45, 135, 90, 270]
-var orientations = [180, 0, 90, 270]
+function scrollTrigger(){
+	if(navArrow) navArrow.style.display = 'none';
+	// endTrigger();
+	removeArrow(navArrow);
+	wheelBuffer = [];
+	currentSession = 1;
+	signalScroll();
+}
+
 
 window.addEventListener('mousewheel', function(e){
 	clearTimeout(lastWheelTimer);
-	lastWheelTimer = setTimeout(endTrigger, 500);
+	if(currentSession == 1 || currentSession == 3){
+		lastWheelTimer = setTimeout(endTrigger, 500);	
+	}else{
+		lastWheelTimer = setTimeout(endTrigger, 1000);	
+	}
+	
 
 	if(currentSession == 0 || currentSession == -1){
 		//to convert wheelDeltas to cartesian, you have to flip it
@@ -231,7 +266,7 @@ window.addEventListener('mousewheel', function(e){
 			// console.log(wheelBuffer)
 			var angles = wheelBuffer.map(function(xy){
 				var x = xy[0], y = xy[1];
-				return (2 * 360 + Math.atan2(y, x) / Math.PI * 180) % 360
+				return (2 * 360 + Math.atan2(y, x) / Math.PI * 180) % 360;
 			});
 			var mean = angles.reduce(function(a, b){return a + b}) / angles.length;
 			var sqerr = angles.map(function(e){ return (e - mean) * (e - mean) });
@@ -240,10 +275,10 @@ window.addEventListener('mousewheel', function(e){
 			var closest = orientations.sort(function(a, b){
 				return Math.pow(mean - a, 2) - Math.pow(mean - b, 2)
 			})[0];
-			console.log(mean, stdev, closest);
+			// console.log(mean, stdev, closest);
 			// console.log(wheelBuffer.length)
-			if(checkpointScroll == lastDetectedScroll && wheelBuffer.length > 0 && stdev < 10 && Math.abs(closest - mean) < 10){
-				console.log('creatin arrow')
+			if(checkpointScroll == lastDetectedScroll && wheelBuffer.length > 0 && stdev < 15 && Math.abs(closest - mean) < 20){
+				// console.log('creatin arrow')
 				navArrow = createArrow();
 				direction = closest;
 				drawArrow(navArrow, closest);
@@ -253,27 +288,20 @@ window.addEventListener('mousewheel', function(e){
 					if(currentSession != 0) return;
 					if(checkpointScroll == lastDetectedScroll){
 						currentSession = 2;
-						// beginTrigger();
-						// Okay, continue.
-						// console.log("CONTINUE")
-						// isTriggering = 2;
-						// beginTrigger();
 						for(var i = 0; i < wheelBuffer.length; i++){
 							mouseWheel(wheelBuffer[i][0], wheelBuffer[i][1]);
 						}
 						wheelBuffer = [];
 					}else{
-						if(navArrow) navArrow.style.display = 'none';
-						endTrigger();
-						currentSession = 1;
-						// isTriggering = 0;
-						// wheelBuffer = []; //clear buffer
-						// console.log("ABORT")
+
+						scrollTrigger()
 						//ABORT ABORT ABORT, the wheeling triggered a scroll
 					}
 				}, 10); // empirically, the margin is usually only about 3msecs
 				// so having an order of magnitude's worth in leeway is probably
 				// sufficient	
+			}else{
+				scrollTrigger()
 			}
 			
 		}, 150)
